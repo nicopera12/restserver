68e244da6e8ef5be91b89b10e0ad07caa6dd1326
nicopera <nicolaspera@MacBook-Pro-de-Nicolas.local>
2018-07-29 02:43:11 -0300

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -15,6 +15,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://CafeUser:CafeUser12!@ds259241.mlab.com:59241/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URL_DB = urlDB;
